89bba94e817f472053c0e136ecf6b161d120f24b
Shahzilllll <uplayer773@gmail.com>
2024-03-20 00:18:02 +0500

    i removed ' your'

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{message: "Type your first number", type: "number", name: "firstnumber"}, 
-{message: "Type your second number", type: "number", name: "secondnumber"},
+const answer = await inquirer.prompt([{message: "Type first number", type: "number", name: "firstnumber"}, 
+{message: "Type second number", type: "number", name: "secondnumber"},
 {message: "Select the operator ou would like to use", type:"list",
 choices: ["Addition", "Subtraction", "Multiplication", "Division"],
 name: "operator",}
