7bedbb4091fcf7ba6a90f4dd31341b8fc22bc13e
Shahzilllll <uplayer773@gmail.com>
2024-03-20 00:08:58 +0500

    push my calculator to github

---
+++ main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{message: "Type your first number", type: "number", name: "firstnumber"}, 
+{message: "Type your second number", type: "number", name: "secondnumber"},
+{message: "Select the operator ou would like to use", type:"list",
+choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+name: "operator",}
+]); 
+console.log(answer);
+
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber)
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber)
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber)
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber)
+}
+else {
+    console.log("ERROR 404")
+};
